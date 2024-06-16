90d332186680524139c9c985d7885e6acc558f40
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2024-06-17 00:03:04 +0800

    快写完了交一发

--- consequence_crawler.py
+++ consequence_crawler.py
@@ -1,6 +1,6 @@
 import stock_hist_em as hist_em
-import numpy as np
 import pymysql
+from datetime import timedelta
 
 # 数据库连接配置
 config = {
@@ -8,7 +8,7 @@ config = {
     "port": 3306,
     "user": "root",
     "passwd": "123456",
-    "db": "financial_analysis_for_listed_companies",
+    "db": "falc_pro",
     "charset": "utf8mb4",
 }
 
@@ -21,12 +21,13 @@ try:
 
     # 创建游标
     with connection.cursor() as cursor:
-        # 构建SQL查询语句，选取感兴趣的字段
-        select_query = f"SELECT {', '.join(fields_of_interest)} FROM revenue_data;"
+        # 构建SQL查询语句，使用DISTINCT去除重复行，选取感兴趣的字段
+        select_query = (
+            f"SELECT DISTINCT {', '.join(fields_of_interest)} FROM financial_data;"
+        )
 
         # 执行查询
         cursor.execute(select_query)
-
         # 获取所有数据
         results = cursor.fetchall()
 
@@ -37,8 +38,6 @@ finally:
     # 关闭连接
     if connection:
         connection.close()
-
-print(data_list)
 
 
 def sava_data_to_database(date_table):
@@ -64,12 +63,12 @@ def sava_data_to_database(date_table):
 
     """ 将数据写入数据库 """
     print("开始向数据库写入数据...")
-    table_name = "consequence_db"
+    table_name = "consequence_data"
     fileds = "公司代码, 日期, 值"
     field_values = r"%s, %s, %s"
 
     cursor.executemany(
-        f"INSERT INTO { table_name }({ fileds }) VALUES ({ field_values })",
+        f"INSERT INTO { table_name }({ fileds }) VALUES ({ field_values }) ON DUPLICATE KEY UPDATE 值 = VALUES(值)",
         date_table,
     )
 
@@ -82,13 +81,59 @@ def sava_data_to_database(date_table):
     return
 
 
-df = hist_em.stock_zh_a_hist(
-    "000001",
-    "daily",
-    "20210229",
-    "20220229",
-)
-# 假设你的数据存储在df中
-data = df.values
+corporation = ""
+consequence_list = []
+for item in data_list:
+    if corporation != item[0]:
+        if corporation != "":
+            sava_data_to_database(consequence_list)
+            consequence_list = []
+            print(corporation + "保存成功！")
+        corporation = item[0]
+    date = item[1]
+    formatted_date = item[1].strftime("%Y%m%d")
+    try:
+        df = hist_em.stock_zh_a_hist(
+            item[0],
+            "daily",
+            formatted_date,
+            formatted_date,
+        )
+    except:
+        continue
+    if df.empty:
+        date2 = date + timedelta(days=60)
+        formatted_date2 = date2.strftime("%Y%m%d")
+        try:
+            df2 = hist_em.stock_zh_a_hist(
+                item[0],
+                "daily",
+                formatted_date,
+                formatted_date2,
+            )
+        except:
+            continue
+        if df2.empty:
+            continue
+    bl = False
+    while df.empty:
+        date += timedelta(days=1)
+        formatted_date = date.strftime("%Y%m%d")
+        try:
+            df = hist_em.stock_zh_a_hist(
+                item[0],
+                "daily",
+                formatted_date,
+                formatted_date,
+            )
+        except:
+            bl = True
+            break
+    if bl:
+        continue
+    consequence_list.append((item[0], item[1], df["开盘"][0]))
 
-print(df)
+sava_data_to_database(consequence_list)
+consequence_list = []
+print(corporation + "保存成功！")
+print("数据保存成功！")
--- randomforest.py
+++ randomforest.py
@@ -3,13 +3,236 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
+import pymysql
 import numpy as np
 
-# 假设df是你的DataFrame，'target'是你想要预测的目标列
-# 请根据实际情况替换下面的'df'和'target'
-df = pd.read_csv("data.csv")  # 读取数据集
-X = df.drop("target", axis=1)  # 特征
-y = df["target"]  # 目标变量
+date_list = [
+    "2019-06-30",
+    "2019-09-30",
+    "2019-12-31",
+    "2020-03-31",
+    "2020-06-30",
+    "2020-09-30",
+    "2020-12-31",
+    "2021-03-31",
+    "2021-06-30",
+    "2021-09-30",
+    "2021-12-31",
+    "2022-03-31",
+    "2022-06-30",
+    "2022-09-30",
+    "2022-12-31",
+    "2023-03-31",
+    "2023-06-30",
+    "2023-09-30",
+    "2023-12-31",
+    "2024-03-31",
+]
+
+index_list = [
+    "BS-10",
+    "BS-11",
+    "BS-21",
+    "BS-22",
+    "BS-3",
+    "BS-33",
+    "BS-34",
+    "BS-40",
+    "BS-46",
+    "BS-47",
+    "BS-51",
+    "BS-53",
+    "BS-56",
+    "BS-57",
+    "BS-58",
+    "BS-6",
+    "BS-67",
+    "BS-8",
+    "BS-80",
+    "BS-81",
+    "BS-83",
+    "BS-84",
+    "BS-90",
+    "BS-91",
+    "BS-93",
+    "BS-94",
+    "CF-10",
+    "CF-11",
+    "CF-12",
+    "CF-19",
+    "CF-20",
+    "CF-24",
+    "CF-25",
+    "CF-27",
+    "CF-32",
+    "CF-34",
+    "CF-36",
+    "CF-37",
+    "CF-38",
+    "CF-40",
+    "CF-41",
+    "CF-42",
+    "CF-44",
+    "CF-47",
+    "CF-48",
+    "CF-5",
+    "CF-58",
+    "CF-6",
+    "CF-63",
+    "CF-64",
+    "CF-68",
+    "CF-7",
+    "CF-72",
+    "CF-73",
+    "CF-76",
+    "CF-8",
+    "CF-9",
+    "PS-10",
+    "PS-16",
+    "PS-17",
+    "PS-18",
+    "PS-2",
+    "PS-20",
+    "PS-22",
+    "PS-23",
+    "PS-26",
+    "PS-27",
+    "PS-29",
+    "PS-3",
+    "PS-4",
+    "PS-5",
+    "PS-6",
+    "PS-7",
+    "PS-8",
+    "PS-9",
+]
+
+# 数据库连接配置
+config = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "passwd": "123456",
+    "db": "consequence_db",
+    "charset": "utf8mb4",
+}
+
+
+try:
+    # 创建连接
+    connection = pymysql.connect(**config)
+
+    # 创建游标
+    with connection.cursor() as cursor:
+        # 构建SQL查询语句，使用DISTINCT去除重复行，选取感兴趣的字段
+        select_query = f"SELECT * FROM consequence_data;"
+
+        # 执行查询
+        cursor.execute(select_query)
+        # 获取所有数据
+        results = cursor.fetchall()
+
+        # 将查询结果存储为列表
+        data_list = [row for row in results]
+
+finally:
+    # 关闭连接
+    if connection:
+        connection.close()
+
+config = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "passwd": "123456",
+    "db": "falc_pro",
+    "charset": "utf8mb4",
+}
+
+# 目标字段列表
+fields_of_interest = ["公司代码", "发行价格", "注册资本"]
+corporations_info = {}
+
+try:
+    # 创建连接
+    connection = pymysql.connect(**config)
+
+    # 创建游标
+    with connection.cursor() as cursor:
+        # 构建SQL查询语句，使用DISTINCT去除重复行，选取感兴趣的字段
+        select_query = f"SELECT DISTINCT {', '.join(fields_of_interest)} FROM corporation_information;"
+
+        # 执行查询
+        cursor.execute(select_query)
+        # 获取所有数据
+        results = cursor.fetchall()
+
+        for row in results:
+            corporations_info[row[0]] = [row[1], row[2]]
+
+finally:
+    # 关闭连接
+    if connection:
+        connection.close()
+
+# 目标字段列表
+fields_of_interest = [
+    "公司代码",
+    "每股发行价",
+    "首发前总股本",
+    "首发后总股本",
+    "实际发行量",
+    "实际募集资金合计",
+    "发行费用总额",
+    "募集资金净额",
+    "承销费用",
+]
+issues_info = {}
+
+try:
+    # 创建连接
+    connection = pymysql.connect(**config)
+
+    # 创建游标
+    with connection.cursor() as cursor:
+        # 构建SQL查询语句，使用DISTINCT去除重复行，选取感兴趣的字段
+        select_query = f"SELECT DISTINCT {', '.join(fields_of_interest)} FROM corporation_information;"
+
+        # 执行查询
+        cursor.execute(select_query)
+        # 获取所有数据
+        results = cursor.fetchall()
+
+        for row in results:
+            issues_info[row[0]] = [
+                row[1],
+                row[2],
+                row[3],
+                row[4],
+                row[5],
+                row[6],
+                row[7],
+                row[8],
+            ]
+
+finally:
+    # 关闭连接
+    if connection:
+        connection.close()
+
+X = []
+y = []
+corporation = ""
+corporation_info = []
+for item in data_list:
+    if item[0] != corporation:
+        corporation_info = []
+        corporation_info += corporations_info[item[0]]
+        corporation_info += issues_info[item[0]]
+        corporation = item[0]
+
+X = np.array(X)
+y = np.array(y)
 
 # 划分训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(
